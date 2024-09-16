7bee28b8e403ab2b701655c9b60b392322a1985d
Utkarsh Kumar Patel <utkarshkumar7545@gmail.com>
2024-09-16 23:52:55 +0530

    eighth solution

---
+++ src/server/8-highest-dismissals-by-bowler.js
@@ -0,0 +1,49 @@
+// 8: Find the highest number of times one player has been dismissed by another player
+
+import delivery from "../data/deliveries.json" assert { type: "json" };
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const dataPath = join(__dirname, "../public/output/highestDismissalsByBowler.json");
+
+const highest_Dismissals_By_Bowler = (delivery) => {
+    const result = {};
+
+    for(let value of delivery){
+        const batsman = value.batsman;
+        const bowler = value.bowler;
+        const dismissals = value.dismissal_kind;
+
+        if(dismissals && dismissals !== "run out"){
+            if(!result[batsman]){
+                result[batsman] = {};
+            }
+            if(!result[batsman][bowler]){
+                result[batsman][bowler] = 0;
+            }
+            result[batsman][bowler]++;
+        }
+    }
+    
+    let finalResult = {"Batsman" : "", "Bowler" : "", "Dismissals" : 0};
+
+    for(let batsman in result){
+        for(let bowler in result[batsman]){
+            if(result[batsman][bowler] > finalResult.Dismissals){
+                finalResult.Batsman = batsman;
+                finalResult.Bowler = bowler;
+                finalResult.Dismissals = result[batsman][bowler];
+            }
+        }
+    }
+    return finalResult;
+}
+
+let matchesRecord = highest_Dismissals_By_Bowler(delivery);
+
+fs.writeFileSync(dataPath, JSON.stringify(matchesRecord, null, 2), "utf-8");
+

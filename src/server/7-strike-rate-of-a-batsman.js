817a843325f322662dcb66aa0f86185c857aeedb
Utkarsh Kumar Patel <utkarshkumar7545@gmail.com>
2024-09-16 23:52:37 +0530

    seventh solution

---
+++ src/server/7-strike-rate-of-a-batsman.js
@@ -0,0 +1,57 @@
+// 7: Find the strike rate of a batsman for each season
+
+import match from "../data/matches.json" assert { type: "json" };
+import delivery from "../data/deliveries.json" assert { type: "json" };
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const dataPath = join(__dirname, "../public/output/strikeRateOfABatsman.json");
+
+const strike_Rate_Of_A_Batsman = (match, delivery) => {
+    const result = {};
+    const matchId_Season = {};
+
+    for(let value of match){
+        matchId_Season[value.id] = value.season;
+    }
+
+    for(let value of delivery){
+        const batsman = value.batsman;
+        const runs = parseInt(value.batsman_runs);
+        const balls = 1;
+        const season = matchId_Season[value.match_id];
+
+        if(season){
+            if(!result[season]){
+                result[season] = {};
+            }
+            if(!result[season][batsman]){
+                result[season][batsman] = {"runs" : 0, "balls" : 0};
+            }
+            result[season][batsman].runs += runs;
+            result[season][batsman].balls++;
+        }
+    }
+
+    const strikeRates = {};
+
+    for(let season in result){
+        strikeRates[season] = {};
+        for(let batsman in result[season]){
+            const runs = result[season][batsman].runs;
+            const balls = result[season][batsman].balls;
+            const strikeRate = (runs / balls) * 100;
+            strikeRates[season][batsman] = strikeRate.toFixed(2);
+        }
+    }
+    return strikeRates;
+}
+
+let matchesRecord = strike_Rate_Of_A_Batsman(match, delivery);
+
+fs.writeFileSync(dataPath, JSON.stringify(matchesRecord, null, 2), "utf-8");
+
